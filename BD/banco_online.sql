01c495fb029e42a8eee1c6c9acc6179d87de88ae
IvanSegur06 <ivan06sep@gmail.com>
2023-06-03 10:59:12 +0200

    Update banco_online.sql

--- BD/banco_online.sql
+++ BD/banco_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2023 a las 20:19:42
+-- Tiempo de generación: 03-06-2023 a las 10:58:18
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -36,6 +36,15 @@ CREATE TABLE `cliente` (
   `Password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`Nombre`, `Apellidos`, `DNI`, `Email`, `Teléfono`, `Password`) VALUES
+('David', 'Segura Carmona', '49166814L', 'david@gmail.com', '345565422', '12345'),
+('Ivan ', 'Segura Carmona', '49166815L', 'ivan06sep@gmail.com', '634653950', '1234'),
+('Paula ', 'Gallardo', '50382116I', 'paulag@gmail.com', '648812590', '123456');
+
 -- --------------------------------------------------------
 
 --
@@ -43,10 +52,17 @@ CREATE TABLE `cliente` (
 --
 
 CREATE TABLE `cuenta` (
-  `Num Cuenta` varchar(100) NOT NULL,
+  `Num_Cuenta` varchar(100) NOT NULL,
   `DNI_Cliente` varchar(9) NOT NULL,
   `Saldo` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cuenta`
+--
+
+INSERT INTO `cuenta` (`Num_Cuenta`, `DNI_Cliente`, `Saldo`) VALUES
+('345345', '49166815L', '25');
 
 -- --------------------------------------------------------
 
@@ -60,8 +76,22 @@ CREATE TABLE `transacciones` (
   `Cantidad` decimal(10,0) NOT NULL,
   `Num_cuenta_destino` varchar(100) NOT NULL,
   `Fecha` date NOT NULL,
-  `Descripción` text NOT NULL
+  `Descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `transacciones`
+--
+
+INSERT INTO `transacciones` (`ID_Transaccion`, `Num_Cuenta`, `Cantidad`, `Num_cuenta_destino`, `Fecha`, `Descripcion`) VALUES
+(3, '345345', '10', '345345', '2023-06-02', 'Prueba 2'),
+(9, '345345', '10', '345345', '2023-06-03', 'Prueba'),
+(10, '345345', '5', '345345', '2023-06-03', 'Prueba'),
+(11, '345345', '20', '338589', '2023-06-03', 'Hola'),
+(12, '345345', '12', '338589', '2023-06-03', 'Prueba'),
+(13, '345345', '3', '338589', '2023-06-03', 'Prueba'),
+(14, '345345', '5', '345345257284', '2023-06-03', 'Prueba'),
+(15, '345345', '5', '257284', '2023-06-03', 'Prueba');
 
 --
 -- Índices para tablas volcadas
@@ -79,7 +109,7 @@ ALTER TABLE `cliente`
 -- Indices de la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
-  ADD PRIMARY KEY (`Num Cuenta`),
+  ADD PRIMARY KEY (`Num_Cuenta`),
   ADD KEY `DNI_Cliente` (`DNI_Cliente`);
 
 --
@@ -97,7 +127,7 @@ ALTER TABLE `transacciones`
 -- AUTO_INCREMENT de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  MODIFY `ID_Transaccion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Transaccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
@@ -113,7 +143,7 @@ ALTER TABLE `cuenta`
 -- Filtros para la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  ADD CONSTRAINT `transacciones_ibfk_1` FOREIGN KEY (`Num_Cuenta`) REFERENCES `cuenta` (`Num Cuenta`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `transacciones_ibfk_1` FOREIGN KEY (`Num_Cuenta`) REFERENCES `cuenta` (`Num_Cuenta`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
